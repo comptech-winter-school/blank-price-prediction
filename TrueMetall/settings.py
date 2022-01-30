cd8469bd078e14d58074ab895f82b98f74a50ab0
«Eugene <e.pavlov98@mail.ru>
2022-01-30 15:41:12 +0700

    bugfix

--- TrueMetall/settings.py
+++ TrueMetall/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6pl#5j&1luk-_wpc)dtm(-4hq)(0_&$ul#-!g#ei(33og^s8^2'
+# SECRET_KEY = 'django-insecure-6pl#5j&1luk-_wpc)dtm(-4hq)(0_&$ul#-!g#ei(33og^s8^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
