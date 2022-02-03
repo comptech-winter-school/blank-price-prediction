4d99de9d5a4765bc2c2b968a9e774c840553ccf0
vladimir <vaberof@gmail.com>
2022-02-03 17:08:14 +0700

    Update upload file function
    1. Add function of valid extensions
    2. Fix some html and css mistakes

--- TrueMetallSite/static/TrueMetallSite/js/index.js
+++ TrueMetallSite/static/TrueMetallSite/js/index.js
@@ -15,7 +15,6 @@ function enablePredictButton() {
     predict_button.style.opacity = 1;
   }
 
-
 // check uploaded file
 function uploadedFile() {
     disablePredictButton()
@@ -29,16 +28,26 @@ function uploadedFile() {
     function handleFile() {
           var file = this.files;
           var fileName = this.files[0].name
+          var fileType = this.files[0].type
 
-          if (file.length == 1) {
+          if (file.length == 1 && validateFileType(fileType)) {
+                console.log(fileType)
                 document.getElementById("upload-text").innerHTML =  "Файл: " + fileName
                 enablePredictButton()
               }
           else {
-              disablePredictButton()
+              alert ("Ivalid file extension")
           }
         }
     })
+}
+
+// validate type of upload file
+function validateFileType(fileType) {
+     return (
+     fileType == "application/vnd.ms-excel"
+     || fileType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+     )
 }
 
 function main() {
