10d580a267fdb6b21eace09d3e6daab357391f6a
«Eugene <e.pavlov98@mail.ru>
2022-02-03 23:37:48 +0700

    added page with previous predictions

--- TrueMetallSite/models.py
+++ TrueMetallSite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Predicts(models.Model):
+    datetime = models.DateTimeField('Дата прогноза', auto_now_add=True)
+    one_week_predict = models.FloatField('Прогноз на 1 неделю')
+    two_weeks_predict = models.FloatField('Прогноз на 2 недели')
+    four_weeks_predict = models.FloatField('Прогноз на 4 недели')
+
+    def __str__(self):
+        return str(self.datetime)
+
+    class Meta:
+        verbose_name = 'Predicts'
+        verbose_name_plural = 'Predicts'
