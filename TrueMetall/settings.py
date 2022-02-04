ccc3a8b531c30bd64ad6d69441971c52ec9cda05
vladimir <vaberof@gmail.com>
2022-02-04 16:40:50 +0700

    Fixed media settings

--- TrueMetall/settings.py
+++ TrueMetall/settings.py
@@ -129,8 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "TrueMetallSite/static",
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "TrueMetallSite/media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
